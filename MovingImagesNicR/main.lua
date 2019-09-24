5fee01e0c79c9f429fe20ba593cd94880f186abd
Nic Riscalas <55556014+s454833@users.noreply.github.com>
2019-09-24 14:31:38 -0400

    added background

--- MovingImagesNicR/main.lua
+++ MovingImagesNicR/main.lua
@@ -1,7 +1,10 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: MovingImages
+-- Name: Nicolas Riscalas
+-- Course: ICS2O/3C
+-- This program displays an image that moves across the screen.
 
--- Your code here
+--global vaiables
+scrollSpeed=3
+
+--background image with width and height
+local backgroundImage=display.newImageRect("Images/background.png", 2048, 1536)
