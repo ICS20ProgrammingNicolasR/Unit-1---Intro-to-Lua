a18d574208027bf615de6548312c78500cc6370c
Nic Riscalas <55556014+s454833@users.noreply.github.com>
2019-09-26 16:21:25 -0400

    updated moving images

--- MovingImagesNicR/main.lua
+++ MovingImagesNicR/main.lua
@@ -7,54 +7,7 @@
 scrollSpeed=3
 
 --background image with width and height
-local backgroundImage=display.newImageRect("Images/background.jpg", 2048, 1536)
+local backgroundImage=display.newImageRect("Images/background.png", 2048, 1536)
 
---character image with width and height 
-local melleniumFalcon=display.newImageRect("Images/spaceship.png", 200, 200)
-
---make a random number variable
-local randomNumber=math.random(1,5)
---set image to be transparent
-melleniumFalcon.alpha=0
-
---set the initial x and y position of the space ship
-melleniumFalcon.x=0
-melleniumFalcon.y=display.contentHeight/randomNumber
-
---Function:MoveShip
---Input:this function accepts an event lstener
---Output:none
---Description this function adds the scroll speed ti the x-value of the ship 
-local function MoveShip(event)
-	-- add the scroll speed to the x-value of the ship 
-	melleniumFalcon.x=melleniumFalcon.x+scrollSpeed
-	--change the transparency of the ship every time it moves so that it fades out 
-	melleniumFalcon.alpha=melleniumFalcon.alpha + 0.01
-end
-
---Move ship will be called over and over again
-Runtime:addEventListener("enterFrame", MoveShip)
-
---create a tiefighter 
-local tiefighter=display.newImageRect("images/tiefighterjpg.jpg", 200,200)
-
---set tiefighter transparent
-tiefighter.alpa=0
-
---set initial x and y of tie fighter
-tiefighter.x=500
-tiefighter.y=0
-
---Function:MoveShip
---Input:this function accepts an event lstener
---Output:none
---Description this function adds the scroll speed ti the x-value of the ship 
-local function MoveShip(event)
-	-- add the scrollSpeedto the y value of the ship 
-	tiefighter.y=tiefighter.y+scrollSpeed
-	--change the transparancy of the ship every time it moves so that it fades into the program
-	tiefighter.alpha=tiefighter.alpha+0.01
-end
-
---Move ship will be called over and over again
-Runtime:addEventListener("enterFrame", MoveShip)
+--character image with widh and height 
+local 
