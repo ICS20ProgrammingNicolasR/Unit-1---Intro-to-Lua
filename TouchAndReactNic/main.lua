9ad671f2e96535ff42eff12745805750947f6cc3
Nic Riscalas <55556014+s454833@users.noreply.github.com>
2019-10-02 14:06:35 -0400

    added touch and react

---
+++ TouchAndReactNic/main.lua
@@ -0,0 +1,8 @@
+-- Title: TouchAndReact
+-- Name: Your Name
+-- Course: ICS2O/3C
+--set the backgroucd color
+display.setDefault("background", 15/255, 100/255, 255/255)
+
+--create a button
+local spaceship=display.newImageRect("Images/mellenium falcon.png",198, 96)
